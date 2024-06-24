4537bb69ecdecd697a593192c1a09e38d5cbf3bf
ergotu <ergotu@ergotu.com>
2024-06-24 15:39:27 +0200

    configure DAP

--- config/plugins/debugging/dap.nix
+++ config/plugins/debugging/dap.nix
@@ -1,1 +1,40 @@
-{plugins = {dap = {enable = true;};};}
+{
+  pkgs,
+  mkKey,
+  ...
+}: let
+  inherit (mkKey) mkKeymap;
+in {
+  plugins.dap = {
+    enable = true;
+    adapters = {};
+    signs = {
+      dapBreakpoint = {
+        text = "●";
+        texthl = "DapBreakpoint";
+      };
+      dapBreakpointCondition = {
+        text = "●";
+        texthl = "DapBreakpointCondition";
+      };
+      dapLogPoint = {
+        text = "◆";
+        texthl = "DapLogPoint";
+      };
+    };
+    extensions = {
+      dap-go = {
+        enable = true;
+        delve.path = "${pkgs.delve}/bin/dlv";
+      };
+      dap-ui = {enable = true;};
+      dap-virtual-text = {enable = true;};
+    };
+  };
+  keymaps = [
+    (mkKeymap "n" "<leader>du" "<cmd>lua require('dapui').toggle()<CR>"
+      "Toggle dapui")
+    (mkKeymap "n" "<leader>db" "<cmd>lua require('dap').toggle_breakpoint()<CR>"
+      "Toggle breakpoint")
+  ];
+}
