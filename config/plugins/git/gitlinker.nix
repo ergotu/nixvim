40b7a5b5e5ce70ddf681dec7fe31690eba67f073
Jordi Lambrechts <jordi@intigriti.com>
2024-06-25 06:13:38 +0200

    add gitlinker keymap with description

--- config/plugins/git/gitlinker.nix
+++ config/plugins/git/gitlinker.nix
@@ -1,9 +1,19 @@
-{
+{mkKey, ...}: let
+  inherit (mkKey) mkKeymap;
+in {
   plugins.gitlinker = {
     enable = true;
-    callbacks = {
-      "github.com" = "get_github_type_url";
-      "gitlab.dnm.radiofrance.fr" = "get_github_type_url";
-    };
+    callbacks = {"github.com" = "get_github_type_url";};
   };
+
+  keymaps = [
+    (mkKeymap "n" "<leader>gy"
+      "<cmd>lua require'gitlinker'.get_buf_range_url('n')<cr>"
+      "Copy URL to current line number")
+    (mkKeymap "v" "<leader>gy"
+      "<cmd>lua require'gitlinker'.get_buf_range_url('v')<cr>"
+      "Copy URL to currently selected range")
+    (mkKeymap "n" "<leader>gY" "<cmd>lua require'gitlinker'.get_repo_url()<cr>"
+      "Copy repository homepage URL")
+  ];
 }
