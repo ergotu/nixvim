d3c709856f92695d0e8e9caa2c15194e1ee6ffaf
Jordi Lambrechts <jordi@intigriti.com>
2024-06-27 06:38:45 +0200

    conform: remove unused config

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -6,11 +6,5 @@
       timeoutMs = 500;
     };
     notifyOnError = true;
-    formattersByFt = {
-      liquidsoap = ["liquidsoap-prettier"];
-      python = ["black"];
-      lua = ["stylua"];
-      yaml = ["yamllint" "yamlfmt"];
-    };
   };
 }
