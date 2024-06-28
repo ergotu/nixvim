fcac2b9c6e8278ac18bca775a7f1a30bc3c312e2
Jordi Lambrechts <jordi@intigriti.com>
2024-06-28 08:44:57 +0200

    lang: add python configuration

---
+++ config/lang/python.nix
@@ -0,0 +1,18 @@
+{
+  plugins = {
+    lsp.servers = {
+      ruff.enable = true;
+      ruff-lsp.enable = true;
+    };
+    none-ls.sources = {
+      formatting.black = {
+        enable = true;
+        withArgs = ''
+          { extra_args = { "--fast" }, }
+        '';
+      };
+      diagnostics.mypy.enable = true;
+    };
+    conform.formattersByFt.python = ["black"];
+  };
+}
