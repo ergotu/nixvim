c63920b7c9e6b9cde8f6c86fcd9e1dee291a2fee
Jordi Lambrechts <jordi@intigriti.com>
2024-06-28 09:04:31 +0200

    lang: add code_actions source for nix files

--- config/lang/nix.nix
+++ config/lang/nix.nix
@@ -5,7 +5,8 @@
     lsp.servers.nixd.enable = true;
     none-ls = {
       sources = {
-        formatting.nixfmt.enable = true;
+        code_actions.statix.enable = true;
+        formatting.alejandra.enable = true;
         diagnostics = {
           statix.enable = true;
           deadnix.enable = true;
