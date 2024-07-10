999dd736c4ab147e6578ee3b361b1d658ef6d34e
Jordi Lambrechts <jordi@intigriti.com>
2024-07-10 07:52:43 +0200

    statusline: show recording message

--- config/plugins/statusline/lualine.nix
+++ config/plugins/statusline/lualine.nix
@@ -13,4 +13,20 @@
       right = "";
     };
   };
+  extraConfigLua =
+    # lua
+    ''
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+            {"encoding", "fileformat", "filetype"},
+          },
+        },
+      })
+    '';
 }
